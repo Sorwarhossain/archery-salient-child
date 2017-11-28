2a5bd1bebd11f331230f0055cad3e8bab3591578
Sorwarhossain <limonhossain57@gmail.com>
2017-11-28 13:13:37 +0600

    design login register page

--- functions.php
+++ functions.php
@@ -12,4 +12,31 @@ function salient_child_enqueue_styles() {
    	wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), false, true);
 }
 
-?>
+/**
+ * customize login screen
+ *
+ */
+function archery_custom_login_page() {
+    echo '<style type="text/css">
+        h1 a { background-image:url("'. get_stylesheet_directory_uri().'/images/logo-white.png") !important; height: 120px !important; width: 100% !important; margin: 0 auto !important; background-size: contain !important; }
+		h1 a:focus { outline: 0 !important; box-shadow: none; }
+        body.login { background-image:url("'. get_stylesheet_directory_uri().'/images/Login-banner.jpg") !important; background-repeat: no-repeat !important; background-attachment: fixed !important; background-position: center !important; background-size: cover !important; position: relative; z-index: 999;}
+  		body.login:before { background-color: rgba(0, 0, 0, 0.9); position: absolute; width: 100%; height: 100%; left: 0; top: 0; content: ""; z-index: -1; }
+  		.login form {
+  			background: rgba(255,255,255, 0.2) !important;
+  		}
+		.login form .input, .login form input[type=checkbox], .login input[type=text] {
+			background: transparent !important;
+			color: #ddd;
+		}
+		.login label {
+			color: #DDD !important;
+		}
+		.login #login_error, .login .message {
+			color: #ddd;
+			margin-top: 20px;
+			background: rgba(255,255,255, 0.2) !important;
+		}
+    </style>';
+}
+add_action('login_head', 'archery_custom_login_page');
